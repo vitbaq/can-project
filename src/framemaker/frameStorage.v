ae3728c0e55115f2517714cf7e2d29bf46509803
CBurgardt <capb@cin.ufpe.br>
2017-11-13 00:01:04 -0300

    Fixed CANStorage and TestBench

--- src/framemaker/frameStorage.v
+++ src/framemaker/frameStorage.v
@@ -1,14 +1,13 @@
 module frameStorage
 (
 	input	sp, CAN_RX, isStuff, reset,
+	//output reg [589:0] frame
 	output reg [589:0] frame
 );
 
-	// Declare state register
-	reg		[589:0] frameValue;
 	
 	initial begin
-		frameValue = 0;
+		frame = 0;
 	end
 
 	// Determine the next state synchronously, based on the
@@ -16,21 +15,13 @@ module frameStorage
 	always @ (posedge sp or posedge reset) begin
 		if (reset)
 			begin
-				frameValue = 0;
+				frame = 0;
 			end
 		else if (~isStuff)
 			begin
-				frameValue = frameValue < 1;
-				frameValue = frameValue + CAN_RX;
+				frame = frame << 1;
+				frame = frame + CAN_RX;
 			end
 	end
-
 	
-	// Determine the output based only on the current state
-	// and the input (do not wait for a clock edge).
-	always @ (state or in)
-	begin
-			frame = frameValue;
-	end
-
 endmodule
--- src/framemaker/frameStorage_TB.v
+++ src/framemaker/frameStorage_TB.v
@@ -5,26 +5,32 @@ module frameStorage_TB
 
 	// Declare state register
 	reg	sp_tb, CAN_RX_tb, isStuff_tb, reset_tb;
-	reg [589:0] frame_tb;
+	wire [589:0] frame_tb;
 
 	frameStorage dut(sp_tb, CAN_RX_tb, isStuff_tb, reset_tb, frame_tb);
 	
 	initial
 	begin
-		sp_tb = 1'b0; CAN_RX_tb = 1'b0; isStuff_tb = 1'b0; reset_tb =1'b0; #5
-		sp_tb = 1'b1; CAN_RX_tb = 1'b1; isStuff_tb = 1'b0; reset_tb =1'b0; #5
-		sp_tb = 1'b0; CAN_RX_tb = 1'b1; isStuff_tb = 1'b0; reset_tb =1'b0; #5
-		sp_tb = 1'b1; CAN_RX_tb = 1'b1; isStuff_tb = 1'b0; reset_tb =1'b0; #5
-		sp_tb = 1'b1; CAN_RX_tb = 1'b0; isStuff_tb = 1'b0; reset_tb =1'b0; #5
-		sp_tb = 1'b1; CAN_RX_tb = 1'b1; isStuff_tb = 1'b1; reset_tb =1'b0; #5
-		sp_tb = 1'b1; CAN_RX_tb = 1'b0; isStuff_tb = 1'b0; reset_tb =1'b0; #5
-		sp_tb = 1'b1; CAN_RX_tb = 1'b1; isStuff_tb = 1'b0; reset_tb =1'b0; #5
-		sp_tb = 1'b0; CAN_RX_tb = 1'b0; isStuff_tb = 1'b0; reset_tb =1'b1; #5
+		sp_tb = 0;
+		CAN_RX_tb = 1'b0; isStuff_tb = 1'b0; reset_tb =1'b0; #10
+		CAN_RX_tb = 1'b1; isStuff_tb = 1'b0; reset_tb =1'b0; #10
+		CAN_RX_tb = 1'b1; isStuff_tb = 1'b0; reset_tb =1'b0; #10
+		CAN_RX_tb = 1'b1; isStuff_tb = 1'b1; reset_tb =1'b0; #10
+		CAN_RX_tb = 1'b0; isStuff_tb = 1'b0; reset_tb =1'b0; #10
+		CAN_RX_tb = 1'b0; isStuff_tb = 1'b0; reset_tb =1'b0; #10
+		CAN_RX_tb = 1'b1; isStuff_tb = 1'b1; reset_tb =1'b0; #10
+		CAN_RX_tb = 1'b1; isStuff_tb = 1'b0; reset_tb =1'b0; #10
 		// Valor esperado : 11001 e depois reseta
+		$stop;
+		CAN_RX_tb = 1'b1; isStuff_tb = 1'b0; reset_tb =1'b1; #10
 		
-		sp_tb = 1'b1; CAN_RX_tb = 1'b1; isStuff_tb = 1'b0; reset_tb =1'b0; #5
-		sp_tb = 1'b1; CAN_RX_tb = 1'b1; isStuff_tb = 1'b0; reset_tb =1'b0; #5
+		CAN_RX_tb = 1'b1; isStuff_tb = 1'b0; reset_tb =1'b0; #10
+		CAN_RX_tb = 1'b1; isStuff_tb = 1'b0; reset_tb =1'b0; #10
 		// Valor esperado : 11
 		$stop;
 	end
+	
+	always
+		#5 sp_tb = !sp_tb;
+		
 endmodule
